59a47c8d57c244f15b546bfaf276a85ff47dc5fb
lexcomesdev <57563562+lexcomesdev@users.noreply.github.com>
2022-02-08 12:46:36 +0100

    paths

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', () => {
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('../serviceworker.js')
+    navigator.serviceWorker.register('./serviceworker.js')
       .then((registration) => {
         console.log('[AppService] ServiceWorker registration successful:', registration);
       })
